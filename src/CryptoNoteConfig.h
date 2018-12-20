92f4c48f8daa23974b7160436f264a626e6d2334
h4ckii <42840968+h4ckii@users.noreply.github.com>
2018-12-20 11:40:50 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -29,7 +29,7 @@ namespace parameters {
   const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 30; //
 
   // MONEY_SUPPLY - total number coins to be generated
-  const uint64_t MONEY_SUPPLY                                  = (uint64_t)(-1)
+  const uint64_t MONEY_SUPPLY                                  = (uint64_t)(-1);
   const uint32_t MANDATORY_TRANSACTION                         = 0;
   const uint32_t KILL_HEIGHT                                   = 0;
   const uint64_t TAIL_EMISSION_REWARD                          = 0;
